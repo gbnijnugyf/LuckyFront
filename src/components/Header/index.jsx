0c94ad548387b62977d7408ab14c3069604058bf
Lruler <1204312199@qq.com>
2021-08-13 00:13:07 +0800

    headercomp

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css' 
+
+export default function Header() {
+    return (
+        <div className='comp-header'>
+            这是头部组件
+        </div>
+    )
+}
---
+++ src/components/RulerButton/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RulerButton() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
