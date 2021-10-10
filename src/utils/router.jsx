80d0bd5e9b791269f842e1dd0b5077e1ef64fef9
DG_Gua <DG_Gua>
2021-10-10 15:22:41 +0800

    [fix]: 全局布局问题

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -15,15 +15,17 @@ export default function Router() {
     return (
         <>
             {isLogin ? <Header></Header> : null}
-            <Switch>
-                <Route path='/login' component={Login}></Route>
-                <Route path='/home' component={Home}></Route>
-                <Route path='/send' component={Send}></Route>
-                <Route path='/detail' component={Detail}></Route>
-                <Route path='/wish' component={Wishes}></Route>
-                <Route path='/mywish' component={MyWish}></Route>
-                <Redirect to={isLogin ? '/home' : '/login'}></Redirect>
-            </Switch>
+            <div className="content">
+                <Switch>
+                    <Route path='/login' component={Login}></Route>
+                    <Route path='/home' component={Home}></Route>
+                    <Route path='/send' component={Send}></Route>
+                    <Route path='/detail' component={Detail}></Route>
+                    <Route path='/wish' component={Wishes}></Route>
+                    <Route path='/mywish' component={MyWish}></Route>
+                    <Redirect to={isLogin ? '/home' : '/login'}></Redirect>
+                </Switch>
+            </div>
         </>
     )
 }
