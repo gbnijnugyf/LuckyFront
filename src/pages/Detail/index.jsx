3e2be363d167614c1a371a04ae93349f858f268d
DG_Gua <DG_Gua>
2021-10-18 08:21:46 +0800

    [refactor]: detail完全重构

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { ButtonS } from '../../components/Button'
+import ConfirmPanel from '../../components/ConfirmPanel'
 
 import './index.scss'
 
@@ -47,117 +48,119 @@ const PersonMsg = () => {
     )
 }
 
-const MsgBorad = (props) => {
-    const { handleAlert } = props
-
+const AbandonBoard = (props) => {
     return (
-        <div className='msg-borad'>
-            <div className="msg-content">
-                <p>你想要放弃这个愿望,</p>
-                <p>建议给对方留言说明原因哦 ：</p>
-                <br />
-                <br />
-                <div className='options'>
-                    <div><input type="radio" name='msg' value='' /></div>
-                    <p>刚刚误触了点亮按钮，不好意思啦～</p>
-                </div>
-                <br />
-                <div className='options'>
-                    <div> <input type="radio" name='msg' value='aa' /></div>
-                    <p>最近有点忙，短时间没有精力实现愿望了，抱歉</p>
-                </div>
-                <br />
-                <div className='options'>
-                    <div><input type='radio' name='msg' value='aa' /></div>
-                    <p>
-                        留言给对方：
-                        <input type="text" placeholder='输入其他原因' className='reson' />
-                    </p>
+        <div className='msg-borad'><p>你想要放弃这个愿望，<br />建议给对方留言说明原因哦：</p>
+            <div className='options'>
+                <div><input type="radio" name='msg' value='' /></div>
+                <p>刚刚误触了点亮按钮，不好意思啦～</p>
+            </div>
+            <div className='options'>
+                <div> <input type="radio" name='msg' value='aa' /></div>
+                <p>最近有点忙，短时间没有精力实现愿望了，抱歉</p>
+            </div>
+            <div className='options'>
+                <div><input type='radio' name='msg' value='aa' /></div>
+                <div>
+                    <p>留言给对方：</p>
+                    <input type="text" placeholder='输入其他原因' className='reason' />
                 </div>
             </div>
-            <div className='no-msg' onClick={() => handleAlert('不留言')}>不留言</div>
-            <div className='sure-msg'>发送</div>
         </div>
     )
 }
 
 
-function Detail() {
+export default function Detail() {
 
+    const [showConfirm, setShowConfirm] = useState(false) // 设置遮罩状态
+    const [confirmContent, setConfirmContent] = useState('') // 设置弹窗内容
+    const [btnText, setBtnText] = useState({}); // 设置按钮文本
+    const [confirmAction, setConfirmAction] = useState({}); // 设置按钮触发
 
-    const [isAlert, setIsAlert] = useState(false) // 设置遮罩状态
-    const [content, setContent] = useState({ text: '', isLong: false }) // 设置弹窗内容
-    const [isSure, setIsSure] = useState(false) // 设置愿望状态
-    const [borad, setBorad] = useState(false) //设置留言板
-
-    const handleAlert = (newContent) => {
-        //  设置遮罩
-        setIsAlert(!isAlert)
-        // 判断弹窗内容设置样式
-        if (typeof newContent === 'string') {
-            if (newContent.length > 15) setContent({ text: newContent, isLong: true })
-            else if (newContent === '不留言') {
-                setIsAlert(false)
-                setBorad(false)
+    // 确认愿望实现
+    const pressAchieve = () => {
+        setConfirmAction(() => {
+            confirmAction.yes = () => {
+                setShowConfirm(false);
+                //TODO: 确认实现愿望后续操作
             }
-            else setContent({ text: newContent, isLong: false })
-        }
-        else if (typeof newContent === 'number' && newContent === 1)
-            confirmSure()
+            confirmAction.no = () => {
+                setShowConfirm(false)
+            }
+            return confirmAction;
+        })
+        setConfirmContent(
+            <>
+                <p style={{ alignSelf: "flex-start" }}>
+                    确认已经实现这个愿望了嘛？
+                </p>
+                <p style={{ alignSelf: "flex-start", textAlign: "start" }}>
+                    若确认，我们将发邮件提醒TA来确认你已经实现了TA的愿望
+                </p>
+            </>
+        );
+        setShowConfirm(true);
+
+    }
+    // 点击放弃愿望
+    const pressAbandon = () => {
+        setConfirmAction(() => {
+            confirmAction.yes = pressReallyAbandon;
+            confirmAction.no = () => {
+                setShowConfirm(false)
+            }
+            return confirmAction;
+        })
+        setConfirmContent(
+            <p>确认放弃这个愿望吗？</p>
+        )
+        setShowConfirm(true);
     }
 
-    const confirmSure = () => {
-        switch (content.text) {
-            case '确认已经实现这个愿望了嘛?\n若确认，我们将发邮件提醒TA来确认你已经实现了TA的愿望':
-                setIsSure(true)
-                break;
+    // 点击确定放弃
+    const pressReallyAbandon = () => {
 
-            case '确认放弃这个愿望吗?':
-                setIsAlert(true)
-                setBorad(true)
-                break;
-
-            case '确认要删除这个愿望':
-                break;
-
-
-            case '确认愿望已经实现了吗':
-                break
-            default:
-        }
+        setConfirmAction(() => {
+            confirmAction.yes = () => {
+                setShowConfirm(false);
+                setBtnText({});
+                //TODO: 放弃愿望结果过程
+            }
+            confirmAction.no = () => {
+                setShowConfirm(false);
+                setBtnText({});
+                //TODO: 不发送留言
+            }
+            return confirmAction;
+        })
+        setConfirmContent(
+            <AbandonBoard />
+        )
+        setBtnText({
+            yes: "发送",
+            no: "不留言"
+        })
     }
-
 
     return (
         <div className='Detail'>
             <WishDetail />
-            <div className='alert' style={{ display: isAlert ? 'block' : 'none' }}></div>
-            {borad ? <MsgBorad handleAlert={handleAlert} /> : null}
-            <div className="content" style={{ display: isAlert ? borad ? 'none' : 'block' : 'none' }}>
-                <div className={content.isLong ? 'long-text' : 'text'}>
-                    <span>{content.text}</span>
-                </div>
-                <div className="sure" onClick={() => handleAlert(1)}>确认</div>
-                <div className="cancel" onClick={() => handleAlert(0)}>取消</div>
-            </div>
             <div className="panel-button">
-                <ButtonS onClick={() => { handleAlert('确认放弃这个愿望吗?') }}
+                <ButtonS onClick={pressAbandon}
                     style={{ background: "#FFFFFF", color: "#F25125", width: "6em" }}>
                     删除这个心愿
                 </ButtonS>
-                <ButtonS onClick={() => { handleAlert(`确认已经实现这个愿望了嘛?\n若确认，我们将发邮件提醒TA来确认你已经实现了TA的愿望`) }}
+                <ButtonS onClick={pressAchieve}
                     style={{ background: "#FF7A59", color: "#FFFFFF", width: "6em", marginLeft: "2em" }}>
                     确认实现
                 </ButtonS>
             </div>
             <hr />
             <PersonMsg />
+            <ConfirmPanel display={showConfirm} action={confirmAction} btnText={btnText} >
+                {confirmContent}
+            </ConfirmPanel>
         </div >
     )
 }
-
-
-
-
-
-export default Detail
