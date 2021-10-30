f9147a6389eae20e3a54092862e2d9c4574c7e91
DG_Gua <DG_Gua>
2021-10-30 17:23:37 +0800

    [feature]: 分两部分的list框架（未关注内容、样式

--- src/pages/MyWish/list.jsx
+++ src/pages/MyWish/list.jsx
@@ -5,13 +5,67 @@ import { ButtonS } from '../../components/Button'
 import Service from '../../common/service'
 
 
-export function MyWishList(props) {
+function WishItem(props) {
+    const { wish } = props
 
-    let [wishes, setWishes] = useState([])
     const wishState = ['待实现', '已实现']
     const goWishDetail = (id) => {
-        props.history.push('/mywish/detail', { id: id })
+        props.history.push('/mywish/detail/' + wish.id)
     }
+
+    return (
+        <li className="item-wish" key={wish.wish_id} onClick={goWishDetail}>
+            <p className="text-detail">{wish.wish}</p>
+            <div className="status">
+                <ButtonS style={{
+                    background: "#FFFFFF",
+                    color: wish.status ? "#F25C33" : "#1DCB1D",
+                    fontSize: "medium",
+                    fontFamily: "PingFangSC",
+                    fontWeight: "Bold",
+                    padding: "0 0.5em"
+                }}>
+                    {wishState[wish.state]}</ButtonS>
+                <p className="text-wishtime">{wish.time}</p>
+            </div>
+        </li>
+    )
+
+}
+
+export function MyWishList(props) {
+
+    let [wishes, setWishes] = useState([{
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }, {
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }, {
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }, {
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }, {
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }, {
+        wish_id: 123,
+        wish: "asdkjfhakshdgf",
+        state: 1,
+        time: "akdfjhsdf"
+    }])
 
     useEffect(() => {
         Service.getUserWish().then((res) => {
@@ -23,40 +77,28 @@ export function MyWishList(props) {
         })
     }, [props.history])
 
-
-
-
     return (
         <div>
             <div className="div-wishlist-toppadding" />
             <div className="div-wishlist">
+                <p>我点亮的愿望</p>
                 <ul>
                     {wishes.map(wish => {
-                        return (
-                            <li className="item-wish" key={wish.wish_id} onClick={() => goWishDetail(wish.wish_id)}>
-                                <p className="text-detail">{wish.wish}</p>
-                                <div className="status">
-                                    <ButtonS style={{
-                                        background: "#FFFFFF",
-                                        color: wish.status ? "#F25C33" : "#1DCB1D",
-                                        fontSize: "medium",
-                                        fontFamily: "PingFangSC",
-                                        fontWeight: "Bold",
-                                        padding: "0 0.5em"
-                                    }}>
-                                        {wishState[wish.state]}</ButtonS>
-                                    <p className="text-wishtime">{wish.time}</p>
-                                </div>
-                            </li>
-                        )
+                        return <WishItem wish={wish} />
                     })}
-                </ul>
+                </ul >
+                <p>我实现的愿望</p>
+                <ul>
+                    {wishes.map(wish => {
+                        return <WishItem wish={wish} />
+                    })}
+                </ul >
                 <div className="div-listbottom">
                     <p>你还剩{7 - wishes.length}次实现小幸运的机会哦~</p>
                     <hr></hr>
                     <p>人家也是有底线的</p>
                 </div>
-            </div>
+            </div >
         </div >
     )
 
