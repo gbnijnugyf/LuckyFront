95a3afd95d9cdcbed23866c345873552d2de0ad7
gbnijnugyf <1612517397@qq.com>
2022-08-16 10:14:53 +0800

    [style]: service.ts -- correct-1

--- src/common/service.ts
+++ src/common/service.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { Url } from 'url';
 import 'whatwg-fetch';
 
 // const BASEURL = window.location.href.slice(0, window.location.href.indexOf('/', 10)) + "/api"
@@ -8,6 +9,11 @@ import 'whatwg-fetch';
 // const BASEURL = window.location.href.slice(0, window.location.href.indexOf('/', 10)) + "/api" //部署环境用
 const BASEURL = "http://127.0.0.1:4523/m1/1379753-0-default"
 
+interface GlobalResponse<T> {
+    data: T,
+    msg: string,
+    status: number
+}
 
 function IsToken(props: IConfig): IConfig {
     const token = localStorage.getItem('token');
@@ -16,13 +22,13 @@ function IsToken(props: IConfig): IConfig {
     props.data.token = token;
     return props;
 }
-async function GlobalAxiosToPost(props: IConfig) {
+async function GlobalAxiosToPost<T>(props: IConfig) {
     props = IsToken(props);
-    const response = await axios.post(props.url, props.data);
+    const response = await axios.post<GlobalResponse<T>>(props.url, props.data);
     // console.log(props.data)
     return response;
 }
-async function GlobalAxiosToGet(props: IConfig) {
+async function GlobalAxiosToGet<T>(props: IConfig) {
     props = IsToken(props);
     if (props.interf === "Light") {
 
@@ -30,17 +36,18 @@ async function GlobalAxiosToGet(props: IConfig) {
     else if (props.interf === "Post") {
 
     }
-    const response = await axios.get(props.url)
+    const response = await axios.get<GlobalResponse<T>>(props.url)
     return response;
 }
 
-async function GlobalAxios(props: IConfig) {//
+async function GlobalAxios<T>(props: IConfig) {//
     let response = null;
     if (props.method === "post")
-        response = await GlobalAxiosToPost(props)
+        response = await GlobalAxiosToPost<T>(props)
     else
-        response = await GlobalAxiosToGet(props)
-    // console.log(response)
+        response = await GlobalAxiosToGet<T>(props)
+
+    // const {data} = response;
 
     if (response.statusText === 'OK') {
         return response;
@@ -55,7 +62,7 @@ async function GlobalAxios(props: IConfig) {//
         // window.location.href(redirectpos)
         window.location.href = redirectpos
     }
-    else if (response.status !== 0) {
+    else if (response.data.status !== 0) {
         alert(response.data.msg)
         return response;
     }
@@ -129,7 +136,7 @@ let Service = {
 
     bindEmail(email: string) {
         //console.log("请求1")
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{ email: string }>(toConfig({
             url: new URL(BASEURL + '/user/email'),
             data: {
                 email: email
@@ -141,7 +148,7 @@ let Service = {
     whutLogin() {
         //console.log("请求2")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{ data: {} }>(toConfig({
             url: new URL(BASEURL + '/whutlogin'),
             data: {
                 data: {}
@@ -154,7 +161,10 @@ let Service = {
     ccnuLogin(idcard_number: string, password: string) {
         //console.log("请求3")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{
+            idcard_number: string,
+            password: string
+        }>(toConfig({
             url: new URL(BASEURL + '/ccnulogin'),
             data: {
                 idcard_number: idcard_number,
@@ -169,7 +179,7 @@ let Service = {
         //console.log("请求4")
 
         let url_ = new URL(BASEURL + '/user/email/check')
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{}>(toConfig({
             url: url_,
             data: {},
             method: "post"
@@ -180,7 +190,14 @@ let Service = {
     postWish(name: string, QQ: string, weChat: string, tel: string, wish: string, type: string) {
         //console.log("请求5")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{
+            wishMan_name: string,
+            wishMan_QQ: string,
+            wishMan_Wechat: string,
+            wishMan_Tel: string,
+            wish: string,
+            type: string
+        }>(toConfig({
             url: new URL(BASEURL + '/wishes/add'),
             data: {
                 wishMan_name: name,
@@ -198,7 +215,13 @@ let Service = {
     lightWishOn(id: string, name: string, tel: string, qq: string, wechat: string) {
         //console.log("请求6")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{
+            wish_id: string,
+            light_name: string,
+            light_tel: string,
+            light_qq: string,
+            light_wechat: string
+        }>(toConfig({
             url: new URL(BASEURL + '/wishes/light'),
             data: {
                 wish_id: id,
@@ -219,7 +242,7 @@ let Service = {
         let url = new URL(BASEURL + '/wishes/details');
         url.searchParams.append("wish_id", id);
         url.searchParams.append("time", (new Date().getTime()).toString());
-        return GlobalAxios(toConfig({ url: url, data: url, method: "get" }));
+        return GlobalAxios<URL>(toConfig({ url: url, data: url, method: "get" }));
     },
 
     //查找点亮人信息
@@ -229,7 +252,7 @@ let Service = {
         let url = new URL(BASEURL + '/user/info/lightman')
         url.searchParams.append("wish_id", id)
         url.searchParams.append("time", (new Date().getTime()).toString())
-        return GlobalAxios(toConfig({ url: url, data: url, method: "get" }))
+        return GlobalAxios<URL>(toConfig({ url: url, data: url, method: "get" }))
     },
 
     //获取自己点亮的愿望//后端接口重构Ligth
@@ -239,7 +262,7 @@ let Service = {
         let url = new URL(BASEURL + '/wishes/user/light')// '/wishes/user/light' or '/desires/user/light'
         url.searchParams.append("time", (new Date().getTime()).toString())
         // console.log(toConfig({ url: url, data: url, method: "get" }))
-        return GlobalAxios(toConfig({ url: url, data: url, method: "get", interf: "Light" }))
+        return GlobalAxios<URL>(toConfig({ url: url, data: url, method: "get", interf: "Light" }))
 
     },
 
@@ -249,7 +272,7 @@ let Service = {
 
         let url = new URL(BASEURL + '/wishes/user/post')
         url.searchParams.append("time", (new Date().getTime()).toString())
-        return GlobalAxios(toConfig({ url: url, data: url, method: "get", interf: "Post" }))
+        return GlobalAxios<URL>(toConfig({ url: url, data: url, method: "get", interf: "Post" }))
     },
 
     //根据分类获取愿望
@@ -260,7 +283,7 @@ let Service = {
         url.searchParams.append("categories", category)
         url.searchParams.append("time", (new Date().getTime()).toString())
         // console.log(toConfig({ url: url, data: url, method: "get" }))
-        return GlobalAxios(toConfig({ url: url, data: url, method: "get" }))
+        return GlobalAxios<URL>(toConfig({ url: url, data: url, method: "get" }))
     },
 
     //删除愿望
@@ -277,7 +300,10 @@ let Service = {
     giveUpLightWish(wish_id: string, msg: string) {
         //console.log("请求13")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{
+            wish_id:string,
+            message:string
+        }>(toConfig({
             url: new URL(BASEURL + `/wishes/giveup`),
             data: {
                 wish_id: wish_id,
@@ -291,7 +317,9 @@ let Service = {
     achieveWish(wish_id: string) {
         //console.log("请求14")
 
-        return GlobalAxios(toConfig({
+        return GlobalAxios<{
+            wish_id:string
+        }>(toConfig({
             url: new URL(BASEURL + `/wishes/achieve`),
             data: {
                 wish_id: wish_id
