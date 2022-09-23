4cd523d65fd234b5b70267aacfd8445fb4b738a8
gbnijnugyf <1612517397@qq.com>
2022-09-23 21:07:23 +0800

    replace confirmPannelBG

--- src/pages/Detail/notfound.tsx
+++ src/pages/Detail/notfound.tsx
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "./index.scss";
 
 export function Notfound() {
+  const navigate = useNavigate();
+
+
+  setTimeout(() => {
+    navigate("/detail/list");
+  }, 5000)
+
+
+
   return (
     <>
+    {/* <div className="nav">5s后自动跳转</div> */}
       <p className="notfound">
         这个愿望不见了呢
         <br />~
